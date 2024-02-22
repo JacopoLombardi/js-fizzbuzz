a033dab23fb7fbebdfef5e3699c9646ef5cf0a4c
JacopoLombardi <lombajacopo@gmail.com>
2024-02-22 16:05:08 +0100

    [add] Finished

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
+// variabili
+let row=document.querySelector('.row');
 
-row=document.querySelector('.row');
 
-
-for(i=1; i<=50; i++){
+// inizio ciclo For
+for(let i=1; i<=100; i++){
 
    if((i % 3 === 0) & (i % 5 === 0)){  
       console.log('FizzBuzz');
-
-      row.innerHTML += `<div class="box">FizzBuzz</div>`;
+      row.innerHTML += `<div class="box red">FizzBuzz</div>`;
 
    }
    else if(i % 3 === 0){
       console.log('Fizz');
-
-      row.innerHTML += `<div class="box">Fizz</div>`;
+      row.innerHTML += `<div class="box green">Fizz</div>`;
 
    }
    else if(i % 5 === 0){
       console.log('Buzz');
-
-      row.innerHTML += `<div class="box">Buzz</div>`;
+      row.innerHTML += `<div class="box yellow">Buzz</div>`;
 
    }
    else{
       console.log(i);
-
-      row.innerHTML += `<div class="box">${i}</div>`;
+      row.innerHTML += `<div class="box cyan">${i}</div>`;
 
    }
 }
